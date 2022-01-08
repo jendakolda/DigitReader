4358e3093ffbc60e56ad27041237853fe544f34c
Já <email@jendakolda.cz>
2022-01-08 22:20:58 +0100

    Stage 2 finished

--- Classification of Handwritten Digits/task/analysis.py
+++ Classification of Handwritten Digits/task/analysis.py
@@ -1,30 +1,46 @@
 # write your code here
 import numpy as np
+import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
 
 class Reader:
 
-    def __init__(self):
-        (self.x_train, self.y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-        assert self.x_train.shape == (60000, 28, 28)
-        # assert x_test.shape == (10000, 28, 28)
-        assert self.y_train.shape == (60000,)
-        # assert y_test.shape == (10000,)
-        # self.flat = np.empty([self.x_train.shape[0], self.x_train.shape[1] * self.x_train.shape[2]], dtype=int)
-        self.flat = self.x_train.reshape(60000, 28 * 28)
+    def __init__(self, REDUCED_SIZE=None):
+        (self.x, self.y), (_, _) = tf.keras.datasets.mnist.load_data()
+        assert self.x.shape == (60000, 28, 28)
+        assert self.y.shape == (60000,)
+        self.x = self.x.reshape(60000, 28 * 28)
+        # While we load the whole set of data, the following program only uses 1/10 of it
+        if REDUCED_SIZE:
+            self.x = self.x[:REDUCED_SIZE, :]
+            self.y = tuple(self.y[:REDUCED_SIZE])
+
+        self.x_train, self.x_test, self.y_train, self.y_test = None, None, None, None
 
     def printer(self):
-        print(f'Classes: {list(set(self.y_train))}')
-        print(f'Features\' shape: {self.flat.shape}')
-        print(f'Target\'s shape: {self.y_train.shape}')
-        print(f'min: {self.flat.min()}, max: {self.flat.max()}')
+        print(f'x_train shape: {self.x_train.shape}')
+        print(f'x_test shape: {self.x_test.shape}')
+        print(f'y_train shape: {len(self.y_train)}')
+        print(f'y_test shape: {len(self.y_test)}')
+        print('Proportion of samples per class in train set:')
+        ds = pd.Series(self.y_train)
+        freq = ds.value_counts(normalize=True)
+        print(freq)
+
+    def splitter(self, set_size, RANDOM_SEED):
+        """Split the set of data into a training and testing domain
+        details cf.
+        https://machinelearningmastery.com/train-test-split-for-evaluating-machine-learning-algorithms/"""
+        self.x_train, self.x_test, self.y_train, self.y_test = \
+            train_test_split(self.x, self.y, test_size=set_size, random_state=RANDOM_SEED)
 
 
 if __name__ == '__main__':
-    digit_reader = Reader()
+    RANDOM_SEED = 40  # for reproducibility of the results
+    set_size = 0.3
+    REDUCED_SIZE = 6000
+    digit_reader = Reader(REDUCED_SIZE)
+    digit_reader.splitter(set_size, RANDOM_SEED)
     digit_reader.printer()
-
-
-
-
